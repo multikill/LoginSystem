abd4234f3b6ac271d6e9d1487b6136622a32141c
astron-dev <astroncsgo@gmail.com>
2017-02-26 02:09:27 +0100

    Updated to MySQLi

--- Register.php
+++ Register.php
@@ -3,10 +3,10 @@
 if(!@include("Settings.php")){die("Database not set up!");}
 
 //Variables for MySQL
-$user     = mysql_real_escape_string($_GET['user']);
-$pass     = mysql_real_escape_string($_GET['pw5']);
-$email    = mysql_real_escape_string($_GET['email']);
-$hwid     = mysql_real_escape_string($_GET['hwid']);
+$user     = mysqli_real_escape_string($_GET['user']);
+$pass     = mysqli_real_escape_string($_GET['pw5']);
+$email    = mysqli_real_escape_string($_GET['email']);
+$hwid     = mysqli_real_escape_string($_GET['hwid']);
 
 if ($verb) {
     if ($email == "" or $user == "" or $pass == "") {
@@ -21,14 +21,14 @@ if ($verb) {
     } else {
         $request_email = "SELECT email FROM ".$dbtable." WHERE email='$email'";
         $request_user  = "SELECT username FROM ".$dbtable." WHERE username='$user'";
-        $result_email  = mysql_query($request_email);
-        $result_user   = mysql_query($request_user);
-        if (mysql_num_rows($result_email) > 0 or mysql_num_rows($result_user) > 0) {
+        $result_email  = mysqli_query($request_email);
+        $result_user   = mysqli_query($request_user);
+        if (mysqli_num_rows($result_email) > 0 or mysqli_num_rows($result_user) > 0) {
             die("<br>Errorcode: <b>1</b><br>Username or email already exists");
         } else {
             $sql = "INSERT INTO " . "".$dbtable." (username, password, email, hwid) " . "VALUES ('" . $user . "', '" . $pass . "', '" . $email . "', '" . $hwid . "')";
 
-            $entry = mysql_query($sql);
+            $entry = mysqli_query($sql);
             if ($entry) {
                 die('<br>FINISHED');
             } else {
@@ -40,5 +40,5 @@ if ($verb) {
     die('<br>Errorcode: <b>6</b>');
 }
 
-mysql_close();
+mysqli_close();
 ?>
