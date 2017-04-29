a1fa367ebe3d10625a1caa3db1f693d8c31fd699
astron-dev <astroncsgo@gmail.com>
2017-04-29 13:45:03 +0200

    Fixed MySQLi

--- Settings.php
+++ Settings.php
@@ -1,13 +1,12 @@
 <?php
 // Made by astron-dev; Pls give credit if youre using my Code.
 // Connection for MySQL
-$dbhost   = "localhost";
-$dbname   = "test";
-$dbuser   = "root";
-$dbpass   = "root";
-$dbtable  = "LoginSystem";
+$DBhost   = "localhost";
+$DBname   = "test";
+$DBuser   = "root";
+$DBpass   = "root";
+$DBtable  = "LoginSystem";
 
 //MySQL Connection
-mysqli_connect($dbhost, $dbuser, $dbpass) or die("Could not connect: " . mysqli_error());
-$verb = mysqli_select_db($dbname);
+$DBcon = new MySQLi($DBhost,$DBuser,$DBpass,$DBname);
 ?>
